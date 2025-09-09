1c90b56b1f7003ae20508fb56130acf1910e07eb
Satsuki Kasuya <satsuki942@gmail.com>
2025-09-09 19:36:15 +0900

    Benchmark: add benchmark targets

--- benchmark/targets/02_dispatch/mvo/Num_sync.py
+++ benchmark/targets/02_dispatch/mvo/Num_sync.py
@@ -1,7 +1,5 @@
-import math
+def _sync_from_v1_to_v2(wrapper_obj):
+    wrapper_obj.value = wrapper_obj.value
 
-def _sync_from_v1_to_v2(v1_impl, v2_impl):
-    v2_impl.value = v1_impl.value
-
-def _sync_from_v2_to_v1(v2_impl, v1_impl):
-    v1_impl.value = v2_impl.value
+def _sync_from_v2_to_v1(wrapper_obj):
+    wrapper_obj.value = wrapper_obj.value
