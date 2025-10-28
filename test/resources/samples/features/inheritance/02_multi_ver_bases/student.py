c5bd8eba1a1af5350ac0aa0b2bb57be9de2c2965
satsuki942 <satsuki942@gmail.com>
2025-10-28 13:30:25 +0900

    Test: add a test case

--- test/resources/samples/features/inheritance/02_multi_ver_bases/student.py
+++ test/resources/samples/features/inheritance/02_multi_ver_bases/student.py
@@ -1,12 +1,12 @@
 from member import Member
-class Student__1__(Member__1__):
+class Student__1__(Member):
     def __init__(self, name, id, major):
         super().__init__(name, id)
         self.major = major
     def introduce(self):
         print(f"Hi, My id is {self.get_id()}.")
 
-class Student__2__(Member__2__):
+class Student__2__(Member):
     def __init__(self, name, id, major):
         super().__init__(name, id)
         self.major = major
